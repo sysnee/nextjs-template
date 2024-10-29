5f967a7568197f3b866be056b6c26fea377c07ec
vilmacio <vilmaciomoura@gmail.com>
2024-10-29 08:13:23 -0300

    add mui joy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import "./globals.css";
+import '@fontsource/inter';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <AppRouterCacheProvider>
         {children}
+      </AppRouterCacheProvider>
       </body>
     </html>
   );
